cd875020468a2e27e79692520c56bb946edfbcd6
lifeng6666 <274837845+lifeng6666@users.noreply.github.com>
2026-06-28 22:50:47 +0800

    test

--- AliV3min.py
+++ AliV3min.py
@@ -209,7 +209,7 @@ class AliV3:
     def getCap(self):
         page = None
         local_html_path = Path(__file__).parent / 'aliv3.html'
-        target_url = local_html_path.as_uri() + "?prefix=1tbpug&SceneId=6mw4mrmg&auto=intercept"
+        target_url = local_html_path.as_uri() + "?prefix=1tbpug&SceneId=6mw4mrmg"
         max_browser_retries = 3
         
         for browser_attempt in range(1, max_browser_retries + 1):
