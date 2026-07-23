b946a658b3c50baa4e045a13581846dca5b66392
lifeng6666 <274837845+lifeng6666@users.noreply.github.com>
2026-07-23 16:18:51 +0800

    50->40

--- jlc.py
+++ jlc.py
@@ -1948,7 +1948,7 @@ def main():
         except:
             continue
 
-    batch = accounts[(account_group-1)*50 : account_group*50]
+    batch = accounts[(account_group-1)*40 : account_group*40]
     argv1 = ",".join([x[0] for x in batch])
     argv2 = ",".join([x[1] for x in batch])
 
